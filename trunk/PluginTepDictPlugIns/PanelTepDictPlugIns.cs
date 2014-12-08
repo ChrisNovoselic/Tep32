4e3454fdbbf3216ed21f1791fa952886e61112da
ChrjapinAN <ChrjapinAN@03528888-2118-ae43-a319-59641b731ea3>
2014-12-08 08:12:45 +0000

    git-svn-id: svn:///U:/Repos/tep32@16 03528888-2118-ae43-a319-59641b731ea3

---
+++ trunk/PluginTepDictPlugIns/PanelTepDictPlugIns.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginTepDictPlugIns
+{
+    public class PanelTepDictPlugIns : HPanelEdit
+    {
+        IPlugIn _iFuncPlugin;
+
+        public PanelTepDictPlugIns(IPlugIn iFunc)
+        {
+            InitializeComponent();
+            this._iFuncPlugin = iFunc;
+        }
+
+        private void InitializeComponent () {
+        }
+    }
+
+    public class PlugIn : HFunc
+    {      
+        public PlugIn () : base () {            
+            _Id = 2;
+
+            _nameOwnerMenuItem = @"Настройка";
+            _nameMenuItem = @"Состав плюгин'ов";
+        }
+
+        public override void OnClickMenuItem(object obj, EventArgs ev)
+        {
+            //if (createObject<PanelTepDictPlugIns>() == true)
+            if (createObject (typeof(PanelTepDictPlugIns)) == true)
+            {
+            }
+            else
+                ;
+        }
+
+        public override void OnEvtDataRecievedHost(object obj)
+        {
+            //throw new NotImplementedException();
+
+            switch (((EventArgsDataHost)obj).id)
+            {
+                default:
+                    break;
+            }
+        }
+    }
+}
