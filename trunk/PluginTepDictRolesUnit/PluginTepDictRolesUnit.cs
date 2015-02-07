e7bc4242e372c25267295480f5496bff03931251
Devil <Devil@03528888-2118-ae43-a319-59641b731ea3>
2015-02-07 04:50:34 +0000

    git-svn-id: svn:///U:/Repos/tep32@32 03528888-2118-ae43-a319-59641b731ea3

---
+++ trunk/PluginTepDictRolesUnit/PluginTepDictRolesUnit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginTepDictRolelesUnit
+{
+    public class PanelTepDictRolesUnit : HPanelEdit
+    {
+        IPlugIn _iFuncPlugin;
+
+        public PanelTepDictRolesUnit(IPlugIn iFunc)
+            : base(@"roles_unit", @"DESCRIPTION")
+        {
+            InitializeComponent();
+            this._iFuncPlugin = iFunc;
+        }
+
+        private void InitializeComponent()
+        {
+        }
+    }
+
+    public class PlugIn : HFuncDictEdit
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 4;
+
+            _nameOwnerMenuItem = @"Настройка";
+            _nameMenuItem = @"Роли(группы) пользователей";
+        }
+
+        public override void OnClickMenuItem(object obj, EventArgs ev)
+        {
+            createObject(typeof(PanelTepDictRolesUnit));
+            //createObject(this.GetType());
+
+            base.OnClickMenuItem(obj, ev);
+        }
+    }
+}
