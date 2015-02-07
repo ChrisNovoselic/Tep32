ccd8bfcf450dcc00a59b4cc83dc35fc9d56cf1cc
Devil <Devil@03528888-2118-ae43-a319-59641b731ea3>
2015-02-07 05:43:39 +0000

    git-svn-id: svn:///U:/Repos/tep32@35 03528888-2118-ae43-a319-59641b731ea3

---
+++ trunk/PluginTepDictProfilesUnit/PluginTepDictProfilesUnit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginTepDictProfilesUnit
+{
+    public class PluginTepDictProfilesUnit : HPanelEdit
+    {
+        IPlugIn _iFuncPlugin;
+
+        public PluginTepDictProfilesUnit(IPlugIn iFunc)
+            : base(@"profiles_unit", @"DESCRIPTION")
+        {
+            InitializeComponent();
+            this._iFuncPlugin = iFunc;
+        }
+
+        private void InitializeComponent()
+        {
+        }
+    }
+
+    public class PlugIn : HFuncDictEdit
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 6;
+
+            _nameOwnerMenuItem = @"Настройка";
+            _nameMenuItem = @"Элементы интерфейса";
+        }
+
+        public override void OnClickMenuItem(object obj, EventArgs ev)
+        {
+            createObject(typeof(PluginTepDictProfilesUnit));
+            //createObject(this.GetType());
+
+            base.OnClickMenuItem(obj, ev);
+        }
+    }
+}
