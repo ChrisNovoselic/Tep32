c9f1c8644778e4cef3af59a3ad66cc92356ae81e
Chrjapin_AN <Chrjapin_AN@03528888-2118-ae43-a319-59641b731ea3>
2016-04-08 07:15:18 +0000

    git-svn-id: svn:///U:/Repos/tep32@230 03528888-2118-ae43-a319-59641b731ea3

---
+++ trunk/PluginDictianary/PanelDictPlugIns.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginDictianary
+{
+    public class PanelDictPlugIns : HPanelEditList
+    {
+        public PanelDictPlugIns(IPlugIn iFunc)
+            : base(iFunc, @"plugins", @"ID", @"DESCRIPTION")
+        {
+            InitializeComponent();
+
+            //Дополнительные действия при сохранении значений
+            delegateSaveAdding = saveAdding;
+        }
+
+        private void InitializeComponent () {
+        }
+
+        private void saveAdding() { /*дополнительные действия не требуются*/ }
+    }
+}
