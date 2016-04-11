79047b0b5c8eb503c84e1fa1d7a5d2b4b1fb29d6
Chrjapin_AN <Chrjapin_AN@03528888-2118-ae43-a319-59641b731ea3>
2016-04-11 01:37:42 +0000

    После группирования
    git-svn-id: svn:///U:/Repos/tep32@247 03528888-2118-ae43-a319-59641b731ea3

---
+++ branches/autobook_002/PluginAboutTepProgram/FormAboutTepProgram.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PluginAboutTepProgram
+{
+    partial class FormAboutTepProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_pictureBox = new System.Windows.Forms.PictureBox();
+            this.m_btnOk = new System.Windows.Forms.Button();
+            this.m_lblVersion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.m_linkLblAuthor = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.m_pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_pictureBox
+            // 
+            this.m_pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.m_pictureBox.Name = "m_pictureBox";
+            this.m_pictureBox.Size = new System.Drawing.Size(64, 64);
+            this.m_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.m_pictureBox.TabIndex = 0;
+            this.m_pictureBox.TabStop = false;
+            // 
+            // m_btnOk
+            // 
+            this.m_btnOk.Location = new System.Drawing.Point(125, 84);
+            this.m_btnOk.Name = "m_btnOk";
+            this.m_btnOk.Size = new System.Drawing.Size(75, 23);
+            this.m_btnOk.TabIndex = 1;
+            this.m_btnOk.Text = "Ok";
+            this.m_btnOk.UseVisualStyleBackColor = true;
+            this.m_btnOk.Click += new System.EventHandler(this.m_btnOk_Click);
+            // 
+            // m_lblVersion
+            // 
+            this.m_lblVersion.AutoSize = true;
+            this.m_lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m_lblVersion.Location = new System.Drawing.Point(87, 12);
+            this.m_lblVersion.Name = "m_lblVersion";
+            this.m_lblVersion.Size = new System.Drawing.Size(67, 15);
+            this.m_lblVersion.TabIndex = 2;
+            this.m_lblVersion.Text = "Version...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Заказчик: \"Генерация\", НТЭЦ-5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Разработчик: Хряпин А.Н.";
+            // 
+            // m_linkLblAuthor
+            // 
+            this.m_linkLblAuthor.AutoSize = true;
+            this.m_linkLblAuthor.Location = new System.Drawing.Point(226, 59);
+            this.m_linkLblAuthor.Name = "m_linkLblAuthor";
+            this.m_linkLblAuthor.Size = new System.Drawing.Size(98, 13);
+            this.m_linkLblAuthor.TabIndex = 5;
+            this.m_linkLblAuthor.TabStop = true;
+            this.m_linkLblAuthor.Text = "ChrjapinAN@itss.ru";
+            // 
+            // FormAboutTepProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 114);
+            this.Controls.Add(this.m_linkLblAuthor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.m_lblVersion);
+            this.Controls.Add(this.m_btnOk);
+            this.Controls.Add(this.m_pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAboutTepProgram";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "О программе";
+            ((System.ComponentModel.ISupportInitialize)(this.m_pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox m_pictureBox;
+        private System.Windows.Forms.Button m_btnOk;
+        private System.Windows.Forms.Label m_lblVersion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel m_linkLblAuthor;
+    }
+}
---
+++ branches/autobook_002/PluginTaskTepMain/PanelTaskTepInval.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.Common;
+using System.Globalization;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginTaskTepMain
+{
+    public class PanelTaskTepInval : PanelTaskTepValues
+    {
+        /// <summary>
+        /// Конструктор - основной (с параметром)
+        /// </summary>
+        /// <param name="iFunc">Объект для связи с вызывающим приложением</param>
+        public PanelTaskTepInval(IPlugIn iFunc)
+            : base(iFunc, TepCommon.HandlerDbTaskCalculate.TaskCalculate.TYPE.IN_VALUES)
+        {            
+            InitializeComponent();
+            // назначить обработчики для кнопок 'К нормативу', 'К макету'
+            (Controls.Find (INDEX_CONTROL.BUTTON_RUN_PREV.ToString(), true)[0] as Button).Click += new EventHandler (btnRunPrev_onClick);
+            // обработчик 'К макету' - см. в базовом классе
+        }
+
+        private void InitializeComponent()
+        {
+        }
+
+        //protected override System.Data.DataTable m_TableOrigin
+        //{
+        //    get { return m_arTableOrigin[(int)HandlerDbTaskCalculate.INDEX_TABLE_VALUES.SESSION]; }
+
+        //    //set { m_arTableOrigin[(int)INDEX_TABLE_VALUES.SESSION] = value.Copy(); }
+        //}
+
+        //protected override System.Data.DataTable m_TableEdit
+        //{
+        //    get { return m_arTableEdit[(int)HandlerDbTaskCalculate.INDEX_TABLE_VALUES.SESSION]; }
+
+        //    //set { m_arTableEdit[(int)INDEX_TABLE_VALUES.SESSION] = value.Copy(); }
+        //}
+
+        /// <summary>
+        /// Сохранить изменения в редактируемых таблицах
+        /// </summary>
+        /// <param name="err">Признак ошибки при выполнении сохранения в БД</param>
+        protected override void recUpdateInsertDelete(out int err)
+        {
+            err = -1;
+
+            //m_handlerDb.RecUpdateInsertDelete(HandlerDbTaskCalculate.s_NameDbTables[(int)INDEX_DBTABLE_NAME.INVALUES]
+            //    , @"ID_PUT, ID_TIME"
+            //    , m_arTableOrigin[(int)HandlerDbTaskCalculate.INDEX_TABLE_VALUES.ARCHIVE]
+            //    , m_arTableEdit[(int)HandlerDbTaskCalculate.INDEX_TABLE_VALUES.ARCHIVE]
+            //    , out err);
+
+            m_handlerDb.RecUpdateInsertDelete(HandlerDbTaskCalculate.s_NameDbTables[(int)INDEX_DBTABLE_NAME.INVAL_DEF]
+                , @"ID_PUT, ID_TIME"
+                , m_arTableOrigin[(int)TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.DEFAULT]
+                , m_arTableEdit[(int)TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.DEFAULT]
+                , out err);
+        }
+        /// <summary>
+        /// Обработчик события при успешном сохранении изменений в редактируемых на вкладке таблицах
+        /// </summary>
+        protected override void successRecUpdateInsertDelete()
+        {
+            m_arTableOrigin[(int)TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.DEFAULT] =
+                m_arTableEdit[(int)TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.DEFAULT].Copy();
+        }
+        /// <summary>
+        /// Освободить (при закрытии), связанные с функционалом ресурсы
+        /// </summary>
+        public override void Stop()
+        {
+            deleteSession();
+
+            base.Stop();
+        }  
+      
+        /// <summary>
+        /// Установить значения таблиц для редактирования
+        /// </summary>
+        /// <param name="err">Идентификатор ошибки при выполнеинии функции</param>
+        /// <param name="strErr">Строка текста сообщения при наличии ошибки</param>
+        protected override void setValues(DateTimeRange[] arQueryRanges, out int err, out string strErr)
+        {
+            err = 0;
+            strErr = string.Empty;
+
+            Session.New();
+            //Запрос для получения архивных данных
+            m_arTableOrigin[(int)TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.ARCHIVE] = new DataTable();
+            //Запрос для получения автоматически собираемых данных
+            m_arTableOrigin[(int)TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.SESSION] = HandlerDb.GetValuesVar(Type
+                , ActualIdPeriod
+                , CountBasePeriod
+                , arQueryRanges
+                , out err);
+            //Проверить признак выполнения запроса
+            if (err == 0)
+            {
+                //Заполнить таблицу данными вводимых вручную (значения по умолчанию)
+                m_arTableOrigin[(int)TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.DEFAULT] = HandlerDb.GetValuesDef(ActualIdPeriod, out err);
+                //Проверить признак выполнения запроса
+                if (err == 0)
+                    //Начать новую сессию расчета
+                    // , получить входные для расчета значения для возможности редактирования
+                    HandlerDb.CreateSession(
+                        CountBasePeriod
+                        , m_arTableDictPrjs[(int)INDEX_TABLE_DICTPRJ.PARAMETER]
+                        , ref m_arTableOrigin
+                        , new DateTimeRange(arQueryRanges[0].Begin, arQueryRanges[arQueryRanges.Length - 1].End)
+                        , out err, out strErr);
+                else
+                    strErr = @"ошибка получения данных по умолчанию с " + Session.m_rangeDatetime.Begin.ToString()
+                        + @" по " + Session.m_rangeDatetime.End.ToString();
+            }
+            else
+                strErr = @"ошибка получения автоматически собираемых данных с " + Session.m_rangeDatetime.Begin.ToString()
+                    + @" по " + Session.m_rangeDatetime.End.ToString();            
+        }
+
+        /// <summary>
+        /// Обработчик события - изменение значения в отображении для сохранения
+        /// </summary>
+        /// <param name="dgv">Объект, инициировавший событие</param>
+        /// <param name="ev">Аргумент события</param>
+        protected override void onEventCellValueChanged(object dgv, DataGridViewTEPValues.DataGridViewTEPValuesCellValueChangedEventArgs ev)
+        {
+            DataRow[] rowsParameter = null;
+
+            for (TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES indx = (TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.UNKNOWN + 1);
+                indx < TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.COUNT;
+                indx++)
+                if (!(indx == TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.DEFAULT)
+                    || ((indx == TepCommon.HandlerDbTaskCalculate.INDEX_TABLE_VALUES.DEFAULT) && (ev.m_iQuality == TepCommon.HandlerDbTaskCalculate.ID_QUALITY_VALUE.DEFAULT)))
+                {
+                    if ((!(m_arTableEdit[(int)indx] == null))
+                        && (m_arTableEdit[(int)indx].Columns.Contains(@"ID_PUT") == true))
+                    {
+                        rowsParameter = m_arTableEdit[(int)indx].Select(@"ID_PUT=" + ev.m_IdParameter);
+
+                        if (rowsParameter.Length == 1)
+                        {
+                            rowsParameter[0][@"VALUE"] = ev.m_Value;
+                            //rowsParameter[0][@"QUALITY"] = (int)HandlerDbTaskCalculate.ID_QUALITY_VALUE.USER;
+                        }
+                        else
+                            Logging.Logg().Error(@"PanelTaskInval::onEventCellValueChanged (INDEX_TABLE_VALUES=" + indx.ToString() + @") - не найден параметр при изменении значения (по умолчанию) в 'DataGridView' ...", Logging.INDEX_MESSAGE.NOT_SET);
+                    }
+                    else
+                        ; //??? ошибка - таблица не инициализирована ИЛИ таблица не содержит столбец 'ID_PUT'
+                }
+                else
+                    ;
+            
+
+        }
+
+        protected override void onButtonLoadClick()
+        {
+            // вызов 'reinit()'
+            //base.HPanelTepCommon_btnUpdate_Click(obj, ev);
+            // для этой вкладки - требуется просто 'clear'
+            // очистить содержание представления
+            clear();
+            // ... - загрузить/отобразить значения из БД
+            base.onButtonLoadClick();
+        }
+
+        /// <summary>
+        /// Обработчик события - нажатие кнопки "Предварительное действие - К нормативу"
+        /// </summary>
+        /// <param name="obj">Объект, инициировавший событие</param>
+        /// <param name="ev">Аргумент события</param>
+        private void btnRunPrev_onClick(object obj, EventArgs ev)
+        {
+            btnRun_onClick(TepCommon.HandlerDbTaskCalculate.TaskCalculate.TYPE.OUT_TEP_NORM_VALUES);
+        }
+
+        /// <summary>
+        /// Обработчик события - нажатие кнопки "Результирующее действие - К макету"
+        /// </summary>
+        /// <param name="obj">Объект, инициировавший событие</param>
+        /// <param name="ev">Аргумент события</param>
+        protected override void btnRunRes_onClick(object obj, EventArgs ev)
+        {
+            btnRun_onClick(TepCommon.HandlerDbTaskCalculate.TaskCalculate.TYPE.OUT_VALUES);
+        }
+
+        /// <summary>
+        /// Инициировать подготовку к расчету
+        ///  , выполнить расчет
+        ///  , актуализировать таблицы с временными значениями
+        /// </summary>
+        /// <param name="type">Тип требуемого расчета</param>
+        private void btnRun_onClick(TepCommon.HandlerDbTaskCalculate.TaskCalculate.TYPE type)
+        {
+            int err = -1;
+
+            try
+            {
+                // обновить входные значения для расчета
+                HandlerDb.UpdateSession(INDEX_DBTABLE_NAME.INVALUES
+                    , m_TableOrigin
+                    , m_TableEdit
+                    , out err);
+                // выполнить расчет
+                HandlerDb.Calculate(type);
+            }
+            catch (Exception e)
+            {
+                //deleteSession ();
+
+                Logging.Logg().Exception(e, @"PanelTaskTepInval::btnRun_onClick (type=" + type.ToString() + @") - ...", Logging.INDEX_MESSAGE.NOT_SET);
+            }
+            finally
+            {
+                //??? сообщение пользователю
+            }
+        }
+        /// <summary>
+        /// Создать объект - панель с управляющими элементами управления
+        /// </summary>
+        /// <returns></returns>
+        protected override PanelTaskTepCalculate.PanelManagementTaskTepCalculate createPanelManagement()
+        {
+            return new PanelManagementTaskTepInval();
+        }
+        /// <summary>
+        /// Класс для размещения управляющих элементов управления
+        /// </summary>
+        protected class PanelManagementTaskTepInval : PanelManagementTaskTepValues
+        {
+            protected override int addButtonRun(int posRow)
+            {
+                Button ctrl = null;
+                int iRes = posRow;
+                //Расчет - выполнить - норматив
+                ctrl = new Button();
+                ctrl.Name = INDEX_CONTROL.BUTTON_RUN_PREV.ToString();
+                ctrl.Text = @"К нормативу";
+                ctrl.Dock = DockStyle.Fill;
+                this.Controls.Add(ctrl, 4, iRes = 0);
+                SetColumnSpan(ctrl, 4); SetRowSpan(ctrl, 1);
+                //Расчет - выполнить - макет
+                ctrl = new Button();
+                ctrl.Name = INDEX_CONTROL.BUTTON_RUN_RES.ToString();
+                ctrl.Text = @"К макету";
+                ctrl.Dock = DockStyle.Fill;
+                this.Controls.Add(ctrl, 4, iRes = iRes + 1);
+                SetColumnSpan(ctrl, 4); SetRowSpan(ctrl, 1);
+
+                return iRes;
+            }
+        }
+    }
+}
