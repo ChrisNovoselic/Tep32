1398f562884c4976e805481a927bbd03326e514a
Chrjapin_AN <Chrjapin_AN@03528888-2118-ae43-a319-59641b731ea3>
2016-04-12 10:21:56 +0000

    git-svn-id: svn:///U:/Repos/tep32@255 03528888-2118-ae43-a319-59641b731ea3

---
+++ branches/cmd_args_001/PluginProject/PanePrjRolesUnit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginProject
+{
+    public class PanelPrjRolesUnit : HPanelEditList
+    {
+        public PanelPrjRolesUnit(IPlugIn iFunc)
+            : base(iFunc, @"roles_unit", @"ID", @"DESCRIPTION")
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+        }
+    }
+}
---
+++ branches/cmd_args_001/PluginProject/PanelPrjRolesFPanels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data; //DataTable
+using System.Data.Common; //DbConnection
+using System.Windows.Forms; //DataGridView...
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginProject
+{
+    public class PanelPrjRolesFPanels : PanelPrjRolesAccess
+    {
+        public PanelPrjRolesFPanels(IPlugIn iFunc)
+            : base(iFunc, @"roles", @"ID_EXT,ID_FPANEL", @"fpanels", @"IsUse")
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+        }
+    }
+}
