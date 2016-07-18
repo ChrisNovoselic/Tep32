77ad2b6e9ac35bc513f16a2b4b809396b65e7a2e
Pasternak_AS <Pasternak_AS@03528888-2118-ae43-a319-59641b731ea3>
2016-07-18 02:55:19 +0000

    git-svn-id: svn:///U:/Repos/tep32@392 03528888-2118-ae43-a319-59641b731ea3

---
+++ branches/tep_datetimePicker/PluginDictionary/PluginItems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginDictionary
+{
+    public class PlugIn : HFuncDbEdit
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 2;
+
+            register(2, typeof(PanelDictFPanels), @"Настройка", @"Состав вкладок");
+            register(3, typeof(PanelDictTime), @"Настройка", @"Интервалы времени");
+            register(4, typeof(PanelDictMessage), @"Настройка", @"Типы сообщений журнала");
+            register(6, typeof(PanelDictProfilesUnit), @"Настройка", @"Элементы интерфейса");
+            register(10, typeof(PanelDictMeasure), @"Настройка", @"Единицы измерения");
+            register(13, typeof(PanelDictSourceInterface), @"Настройка", @"Интерфейсы источников данных");            
+        }
+
+        public override void OnClickMenuItem(object obj, /*PlugInMenuItem*/EventArgs ev)
+        {
+            base.OnClickMenuItem(obj, ev);
+        }
+    }
+}
---
+++ branches/tep_datetimePicker/PluginTaskTepForm3Tech/PanelTaskTepForm3Tech.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginTaskTepForm3Tech
+{
+    public class PanelTaskTepForm3Tech : HPanelTepCommon
+    {
+        public PanelTaskTepForm3Tech(IPlugIn iFunc)
+            : base(iFunc)
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+        }
+
+        protected override void initialize(out int err, out string errMsg)
+        {
+            err = 0;
+            errMsg = string.Empty;
+        }
+
+        protected override void recUpdateInsertDelete(out int err)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void successRecUpdateInsertDelete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PlugIn : HFuncDbEdit
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 22;
+            register(22, typeof(PanelTaskTepForm3Tech), @"Задача\Расчет ТЭП", @"Форма 3-тех");
+        }
+
+        public override void OnClickMenuItem(object obj, /*PlugInMenuItem*/EventArgs ev)
+        {
+            base.OnClickMenuItem(obj, ev);
+        }
+    }
+}
+
