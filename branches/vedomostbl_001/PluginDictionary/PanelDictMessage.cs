7f6d7e339b4015ee40adb321d5d9f0d5c9e55f80
Pasternak_AS <Pasternak_AS@03528888-2118-ae43-a319-59641b731ea3>
2016-08-04 01:50:29 +0000

    git-svn-id: svn:///U:/Repos/tep32@415 03528888-2118-ae43-a319-59641b731ea3

---
+++ branches/vedomostbl_001/PluginDictionary/PanelDictMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginDictionary
+{
+    public class PanelDictMessage : HPanelEditList
+    {
+        public PanelDictMessage(IPlugIn iFunc)
+            : base(iFunc, @"messages", @"ID", @"DESCRIPTION")
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+        }
+    }
+}
