7fc2c21fc8d76808aa42c7b20d188df4d1d7337a
Pasternak_AS <Pasternak_AS@03528888-2118-ae43-a319-59641b731ea3>
2016-08-23 07:26:39 +0000

    git-svn-id: svn:///U:/Repos/tep32@422 03528888-2118-ae43-a319-59641b731ea3

---
+++ branches/vedomostbl_002/PluginDictionary/PanelDictTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginDictionary
+{
+    public class PanelDictTime : HPanelEditList
+    {
+        public PanelDictTime(IPlugIn iFunc)
+            : base(iFunc, @"time", @"ID", @"DESCRIPTION")
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+        }
+    }
+}
