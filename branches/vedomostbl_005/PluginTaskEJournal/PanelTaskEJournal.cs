80dfc96e2368b3325f458d48349554ef1cedb3a2
Pasternak_AS <Pasternak_AS@03528888-2118-ae43-a319-59641b731ea3>
2016-10-03 02:37:48 +0000

    git-svn-id: svn:///U:/Repos/tep32@456 03528888-2118-ae43-a319-59641b731ea3

---
+++ branches/vedomostbl_005/PluginTaskEJournal/PanelTaskEJournal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using HClassLibrary;
+using TepCommon;
+using InterfacePlugIn;
+
+namespace PluginTaskEJournal
+{
+    public class PanelTaskEJournal : HPanelTepCommon
+    {
+        public PanelTaskEJournal(IPlugIn iFunc)
+            : base(iFunc)
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+        }
+
+        protected override void initialize(out int err, out string errMsg)
+        {
+            err = 0;
+            errMsg = string.Empty;
+        }
+
+        protected override void recUpdateInsertDelete(out int err)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void successRecUpdateInsertDelete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PlugIn : HFuncDbEdit
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 20;
+            register(20, typeof(PanelTaskEJournal), @"Задача", @"Электронный журнал");
+        }
+
+        public override void OnClickMenuItem(object obj, /*PlugInMenuItem*/EventArgs ev)
+        {
+            base.OnClickMenuItem(obj, ev);
+        }
+    }
+}
